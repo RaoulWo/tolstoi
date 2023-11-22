4e308a96f153a5605c1785fbdd9c48c6b2cf7082
Raoul Wograndl <if21b142@technikum-wien.at>
2023-11-22 22:44:25 +0100

    Update text file names, read file content

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,13 @@ mod config;
 
 pub use config::Config;
 
-use std::error::Error;
+use std::{error::Error, fs};
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    // Read the files.
+    let book = fs::read_to_string(&config.book_fp)?;
+    let peace = fs::read_to_string(&config.peace_terms_fp)?;
+    let war = fs::read_to_string(&config.war_terms_fp)?;
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, process};
+
+use tolstoi::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Parsing error: {err}");
+        process::exit(1);
+    });
+
+    if let Err(err) = tolstoi::run(&config) {
+        eprintln!("Application error: {err}");
+        process::exit(1);
+    };
 }
